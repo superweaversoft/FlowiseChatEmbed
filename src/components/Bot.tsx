f7e0d10be865787d7587b56e795b43732cea9de2
Eldar Eini <eldar@weaversoft.io>
2024-07-31 13:53:50 +0300

    remove tag

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1163,7 +1163,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
             poweredByTextColor={props.poweredByTextColor}
             botContainer={botContainer}
           /> */}
-          
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
