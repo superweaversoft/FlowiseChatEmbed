03fd4849332f2f8877222fae95fa8ad7d2a7b2a6
ArtemNikov <artem@weaversoft.io>
2024-12-01 00:24:29 +0200

    Removed localhost

--- src/queries/sendMessageQuery.ts
+++ src/queries/sendMessageQuery.ts
@@ -59,7 +59,7 @@ export type LeadCaptureRequest = BaseRequest & {
   body: Partial<LeadCaptureInput>;
 };
 
-export const sendFeedbackQuery = ({ chatflowid, apiHost = 'http://localhost:3000', body, onRequest }: CreateFeedbackRequest) =>
+export const sendFeedbackQuery = ({ chatflowid, apiHost = 'https://demo-ui2.app.weaversoft.io', body, onRequest }: CreateFeedbackRequest) =>
   sendRequest({
     method: 'POST',
     url: `${apiHost}/api/v1/feedback/${chatflowid}`,
@@ -67,7 +67,7 @@ export const sendFeedbackQuery = ({ chatflowid, apiHost = 'http://localhost:3000
     onRequest: onRequest,
   });
 
-export const updateFeedbackQuery = ({ id, apiHost = 'http://localhost:3000', body, onRequest }: UpdateFeedbackRequest) =>
+export const updateFeedbackQuery = ({ id, apiHost = 'https://demo-ui2.app.weaversoft.io', body, onRequest }: UpdateFeedbackRequest) =>
   sendRequest({
     method: 'PUT',
     url: `${apiHost}/api/v1/feedback/${id}`,
@@ -75,7 +75,7 @@ export const updateFeedbackQuery = ({ id, apiHost = 'http://localhost:3000', bod
     onRequest: onRequest,
   });
 
-export const sendMessageQuery = ({ chatflowid, apiHost = 'http://localhost:3000', body, onRequest }: MessageRequest) =>
+export const sendMessageQuery = ({ chatflowid, apiHost = 'https://demo-ui2.app.weaversoft.io', body, onRequest }: MessageRequest) =>
   sendRequest<any>({
     method: 'POST',
     url: `${apiHost}/api/v1/prediction/${chatflowid}`,
@@ -83,7 +83,7 @@ export const sendMessageQuery = ({ chatflowid, apiHost = 'http://localhost:3000'
     onRequest: onRequest,
   });
 
-export const createAttachmentWithFormData = ({ chatflowid, apiHost = 'http://localhost:3000', formData, onRequest }: UpsertRequest) =>
+export const createAttachmentWithFormData = ({ chatflowid, apiHost = 'https://demo-ui2.app.weaversoft.io', formData, onRequest }: UpsertRequest) =>
   sendRequest({
     method: 'POST',
     url: `${apiHost}/api/v1/attachments/${chatflowid}/${formData.get('chatId')}`,
@@ -94,7 +94,7 @@ export const createAttachmentWithFormData = ({ chatflowid, apiHost = 'http://loc
     onRequest: onRequest,
   });
 
-export const upsertVectorStoreWithFormData = ({ chatflowid, apiHost = 'http://localhost:3000', formData, onRequest }: UpsertRequest) =>
+export const upsertVectorStoreWithFormData = ({ chatflowid, apiHost = 'https://demo-ui2.app.weaversoft.io', formData, onRequest }: UpsertRequest) =>
   sendRequest({
     method: 'POST',
     url: `${apiHost}/api/v1/vector/upsert/${chatflowid}`,
@@ -105,14 +105,14 @@ export const upsertVectorStoreWithFormData = ({ chatflowid, apiHost = 'http://lo
     onRequest: onRequest,
   });
 
-export const getChatbotConfig = ({ chatflowid, apiHost = 'http://localhost:3000', onRequest }: MessageRequest) =>
+export const getChatbotConfig = ({ chatflowid, apiHost = 'https://demo-ui2.app.weaversoft.io', onRequest }: MessageRequest) =>
   sendRequest<any>({
     method: 'GET',
     url: `${apiHost}/api/v1/public-chatbotConfig/${chatflowid}`,
     onRequest: onRequest,
   });
 
-export const isStreamAvailableQuery = ({ chatflowid, apiHost = 'http://localhost:3000', onRequest }: MessageRequest) =>
+export const isStreamAvailableQuery = ({ chatflowid, apiHost = 'https://demo-ui2.app.weaversoft.io', onRequest }: MessageRequest) =>
   sendRequest<any>({
     method: 'GET',
     url: `${apiHost}/api/v1/chatflows-streaming/${chatflowid}`,
@@ -128,7 +128,7 @@ export const sendFileDownloadQuery = ({ apiHost = 'https://demo-ui2.app.weaverso
     onRequest: onRequest,
   });
 
-export const addLeadQuery = ({ apiHost = 'http://localhost:3000', body, onRequest }: LeadCaptureRequest) =>
+export const addLeadQuery = ({ apiHost = 'https://demo-ui2.app.weaversoft.io', body, onRequest }: LeadCaptureRequest) =>
   sendRequest<any>({
     method: 'POST',
     url: `${apiHost}/api/v1/leads/`,
